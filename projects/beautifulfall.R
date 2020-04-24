ddc678dfbb78230e14345c44f16e8b347e04f926
letsang <56046208+letsang@users.noreply.github.com>
2020-04-24 18:54:30 +0200

    Add files via upload

--- projects/beautifulfall.R
+++ projects/beautifulfall.R
@@ -2,6 +2,7 @@ library(pdftools)
 library(dplyr)
 library(stringr)
 library(ggplot2)
+library(shiny)
 
 ### Remove stopwords function ###
 rm_stopw <-function(x)
@@ -11,16 +12,17 @@ rm_stopw <-function(x)
 }
   
 ### Create list of nouns by page ###
-nouns <-pdf_text("/home/tsang/Bureau/beautifulpeople/beautiful_fall.pdf") %>%
-        str_replace_all("\n"," ") %>%
-        str_extract_all(pattern="(((?<![:punct:])[:upper:][:lower:]+[:space:]|\n)+)|\\d{4}") %>%
+nouns <-pdf_text("beautiful_fall.pdf") %>%
+        str_replace_all("\n|\t|\r|\v|\f"," ") %>%
+        lapply(str_replace_all,"[:space:]+"," ") %>%
+        str_extract_all(pattern="(((?<![:punct:])[:upper:][:lower:]+[:space:])+)|\\d{4}") %>%
         lapply(str_trim) %>%
         lapply(rm_stopw)
 
 ### Give page number : p. ###
 names(nouns) <- paste("p.",as.character(1:length(nouns)),sep="")
 
-### Create matrix by number of page ###
+### Create list by number of page function ###
 ma_page <-function(x,i,j)
 {
   page <-list()
@@ -32,7 +34,42 @@ ma_page <-function(x,i,j)
   }
   return(page)
 }
-page <-ma_page(nouns,1,200)
 
-### Plot method ###
-ggplot(page[[9]],aes(x=nouns,y=count)) + geom_col() + coord_flip() + ggtitle(names(page[10]))
+### Create dataframe function
+dtf_page <-function(x,i,j)
+{
+  page <-data.frame(nouns=character(),
+                    count=integer(),
+                    page=character())
+  while(i <= j)
+  {
+    page <- rbind(page,data_frame(nouns=unlist(x[i]),page=names(x[i])) %>%
+      count(nouns,sort=TRUE,name="count") %>% mutate(page=names(x[i])))
+    i = i + 1
+  }
+  return(page)
+}
+
+### The Most Beautiful People ###
+shinyApp(
+  ui <- fluidPage(
+  titlePanel("The Most Beautiful People in Beautiful People"),
+  tags$h4("Splendeurs et Misères de la Mode"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("pages", "Choose the page:",
+                                    value = 42,
+                                    min = 1,
+                                    max = 200)),
+    mainPanel(
+      plotOutput("popular"))
+    )
+  ),
+  
+  server <- function(input, output) {
+  output$popular <- renderPlot({
+  page <- dtf_page(nouns,input$pages,input$pages)
+  beautiful <-page %>% group_by(nouns=as.factor(nouns)) %>% summarise(count=sum(count)) %>% arrange(desc(count))
+  ggplot(beautiful[1:10,], aes(x=reorder(nouns,count), count)) + geom_col(fill="black") + coord_flip() + labs(x="nouns") + ggtitle(paste("The Most Beautiful People in page",as.character(input$pages)))
+  })
+})
